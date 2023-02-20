0a5988824a716c790941df75345f94d67f56fa35
ElenaObukhova1984 <e.obukhova84@gmail.com>
2023-02-20 10:53:53 +0200

    install yargs

---
+++ contacts.js
@@ -0,0 +1,22 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+
+const contactsPath = path.join(__dirname, "contacts.json");
+console.log(contactsPath);  
+
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
